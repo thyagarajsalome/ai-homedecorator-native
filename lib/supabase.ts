867e748a15e2edd8d9613e35b1f01e379c6b2735
https://github.com/thyagarajsalome/KQ-V1.0.0.git <thyagarajsalome@gmail.com>
2025-11-19 14:30:31 +0530

    btn-corrected

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
-
-// Replace with your actual Supabase URL and Anon Key
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+// Import keys from the environment variables
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
