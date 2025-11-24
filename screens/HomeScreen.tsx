57d8d31feb458e91198d2250bf73e665686e9f9e
https://github.com/thyagarajsalome/KQ-V1.0.0.git <thyagarajsalome@gmail.com>
2025-11-24 16:38:09 +0530

    updated

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -198,7 +198,6 @@ const ImageUploader: React.FC<{ onImageSelected: (uri: string) => void }> = ({
       return;
     }
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 0.5,
